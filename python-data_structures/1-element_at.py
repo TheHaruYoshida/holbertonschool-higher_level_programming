19bbc995cd1f3acbba5aac604034240a6323bae6
TheHaruYoshida <santiagovidarte@gmail.com>
2023-01-28 08:53:26 -0800

    fix task 1, idx = 3 case

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    if len(my_list) < idx:
+    if len(my_list) <= idx:
         return None
     else:
         return my_list[idx]
