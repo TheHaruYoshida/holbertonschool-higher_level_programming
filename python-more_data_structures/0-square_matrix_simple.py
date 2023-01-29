cb7b42ac879c3f99976a6c135bdb8271a5480a17
TheHaruYoshida <santiagovidarte@gmail.com>
2023-01-29 09:52:09 -0800

    add task 0, 1 and readme

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+def square_matrix_simple(matrix=[]):
+    new_square = []
+    for x in range(0, len(matrix)):
+        square = [y*y for y in matrix[x]]
+        new_square.append(square)
+    return (new_square)
