1bc77a57600030a54ebcce5d47795e1cca87ac43
TheHaruYoshida <santiagovidarte@gmail.com>
2023-02-01 09:53:06 -0800

    fix documentation task 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -2,6 +2,7 @@
 class Square:
     """ Defines a class Square"""
     def __init__(self, size=0):
+	     """ initializes the Square"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
