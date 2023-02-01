2a2e913024b24d7413e437d37f9ff0c2f35e6621
TheHaruYoshida <santiagovidarte@gmail.com>
2023-02-01 07:12:17 -0800

    add task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Defines a square by size
+    Attributes:
+        __size (int): Size of the square
+    """
+    def __init__(self, size):
+        """ init method
+        Args:
+            size (int): Size of the square
+        """
+        self.__size = size
