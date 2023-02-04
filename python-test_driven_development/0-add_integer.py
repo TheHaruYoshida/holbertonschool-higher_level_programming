358e77e77a93c3f054282f3d58b04f91974d877e
TheHaruYoshida <santiagovidarte@gmail.com>
2023-02-04 09:55:06 -0800

    add task 0, and readme

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""return the result of adding two numbers"""
+
+def add_integer(a, b=98):
+    """ adds two ints """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
