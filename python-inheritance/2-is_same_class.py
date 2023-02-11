0f2179045538a4adaddde45cbf3b3977ef97b8f1
TheHaruYoshida <santiagovidarte@gmail.com>
2023-02-11 11:48:31 -0800

    add task 2 and 3

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Define class Is_same_class """
+
+
+def is_same_class(obj, a_class):
+    """ returns whether object is exactly an instance of the specified class """
+    return type(obj) is a_class
