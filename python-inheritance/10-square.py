fc7945bcb3f9982eccb4d033f5e038ff14450c5c
TheHaruYoshida <santiagovidarte@gmail.com>
2023-02-11 15:15:22 -0800

    fix class documented

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ Define class Square """
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """ Define class square (rectangle) """
     def __init__(self, size):
         self.__size = size
         super().__init__(size, size)
