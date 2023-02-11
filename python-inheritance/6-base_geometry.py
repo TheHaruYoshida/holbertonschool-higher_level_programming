82086aeb95dcb692add74afe724ecdb53378c8c9
TheHaruYoshida <santiagovidarte@gmail.com>
2023-02-11 12:53:50 -0800

    add task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines class BaseGeometry"""
+
+
+class BaseGeometry():
+    """class BaseGeometry"""
+    def area(self):
+        """area Exception"""
+        raise Exception("area() is not implemented")
