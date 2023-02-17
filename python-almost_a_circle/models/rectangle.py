ed0719d8142ba3844135ff1c30c88444a1a07c9c
TheHaruYoshida <santiagovidarte.com>
2023-02-17 08:13:39 -0800

    add task 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" Defines module that contains class Rectangle """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Defines class rectangle """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super(Rectangle, self).__init__(id)
+
+
+    @property
+    def width(self):
+        """ get the width """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """ width setter """
+        self.__width = width
+
+
+    @property
+    def height(self):
+        """ get the height """
+        return self.__height
+
+
+    @height.setter
+    def height(self, width):
+        """ height setter """
+        self.__height = height
+
+    @property
+    def x(self):
+        """ get the x """
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """ x setter """
+        self.__x = x
+
+    @property
+    def y(self):
+        """ get the y """
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """ y setter """
+        self.__y = y
