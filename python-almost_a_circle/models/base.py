2d57843adab3eb3b2345b569a81ca32e11c9a096
TheHaruYoshida <santiagovidarte.com>
2023-02-19 10:54:06 -0800

    fix and add up to task 18

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,21 +19,33 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ returns the JSON string """
-        Jsong = "[]"
-        if (list_dictionaries is not None and
-                list_dictionaries):
-            Jsong = "["
-            for i in list_dictionaries:
-                Jsong += json.dumps(i)
-            Jsong += "]"
-        return Jsong
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """ writes the JSON string representation into file"""
+        """ writes the JSON string representation into file """
         if list_objs is None:
             dict_objs = []
         else:
             dict_objs = [lists.to_dictionary() for lists in list_objs]
         with open(f"{cls.__name__}.json", mode='w', encoding='utf-8') as f:
             f.write(cls.to_json_string(dict_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns the list JSON representation """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns all the attributes already set """
+        if cls.__name__ == "Rectangle":
+            haddo = cls(1, 1)
+        else:
+            haddo = cls(1)
+        haddo.update(**dictionary)
+        return haddo
