768b8ae8f9780ac9efb20982c65ba4a205942907
TheHaruYoshida <santiagovidarte@gmail.com>
2023-04-16 15:51:37 -0700

    fix semistandar from task 8

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    let listreversed = [];
-    for (let i = 0; i < list.length; i++) {
-      listreversed.unshift(list[i]);
-    }
-    return listreversed;
-  };
-  
+  const listreversed = [];
+  for (let i = 0; i < list.length; i++) {
+    listreversed.unshift(list[i]);
+  }
+  return listreversed;
+};
